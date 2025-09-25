4d55a42e22379d50992a7be87aafcc0277186e29
Adil <muhammadaadil221141@gmail.com>
2025-09-25 14:24:46 +0500

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
       ),
       home: SplashScreen(),
     );
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String email = '';
+  String password = '';
+
+  void _login() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print('Email: $email, Password: $password');
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Logging in...')));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login', style: TextStyle(color: Colors.white)),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Email'),
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (value) => email = value!,
+                validator: (value) =>
+                    value!.isEmpty ? 'Please enter your email' : null,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                onSaved: (value) => password = value!,
+                validator: (value) =>
+                    value!.isEmpty ? 'Please enter your password' : null,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(onPressed: _login, child: Text('Login')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
