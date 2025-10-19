6a82e1565b2cf9577f31269486abe95658b7e9ef
Adil <muhammadaadil221141@gmail.com>
2025-10-20 03:46:25 +0500

    ...

--- lib/screens/worker_card.dart
+++ lib/screens/worker_card.dart
@@ -1,5 +1,3 @@
-import 'package:chitral_dost_app/data/workers_data.dart';
-import 'package:chitral_dost_app/screens/worker_profile.dart';
 import 'package:flutter/material.dart';
 
 class WorkerCard extends StatelessWidget {
