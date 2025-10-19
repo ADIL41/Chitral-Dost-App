5b23627170a281b7b53991d9a85ece4ea329d304
Adil <muhammadaadil221141@gmail.com>
2025-10-20 03:45:37 +0500

    ..

--- lib/screens/worker_profile.dart
+++ lib/screens/worker_profile.dart
@@ -1,6 +1,5 @@
 import 'package:chitral_dost_app/models/worker_model.dart';
 import 'package:chitral_dost_app/screens/booking_detail.dart';
-import 'package:chitral_dost_app/screens/bottom_navbar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
