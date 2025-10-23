fd88d6699363258c2d7caed95aaecc192640b846
Adil <muhammadaadil221141@gmail.com>
2025-10-23 23:22:53 +0500

    ...

--- lib/screens/worker_form.dart
+++ lib/screens/worker_form.dart
@@ -1,5 +1,3 @@
-import 'package:chitral_dost_app/data/workers_data.dart';
-import 'package:chitral_dost_app/screens/worker_profile.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
