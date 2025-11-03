0ed3a912e4448150bfd9dfe3448a29f91673cf87
Adil <muhammadaadil221141@gmail.com>
2025-11-04 00:56:55 +0500

    ..

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -116,11 +116,7 @@ class _SignUpState extends State<SignUp> {
                         if (value == null || value.isEmpty) {
                           return "Please enter your email";
                         }
-                        if (!RegExp(
-                          r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
-                        ).hasMatch(value)) {
-                          return "Please enter a valid email address";
-                        }
+
                         return null;
                       },
                     ),
