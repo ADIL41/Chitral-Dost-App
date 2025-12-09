ac8ec24518cd83e5b9542a199dcf96c6bac01afe
Adil <muhammadaadil221141@gmail.com>
2025-12-09 13:43:22 +0500

    update profile screen with firebase firestore

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -6,6 +8,8 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final User? currentUser = FirebaseAuth.instance.currentUser;
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -21,62 +25,77 @@ class ProfileScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.teal.shade200,
-                child: Icon(Icons.person, size: 60, color: Colors.black),
-              ),
-              const SizedBox(height: 16),
-              Text(
-                'Customer Name',
-                style: GoogleFonts.poppins(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-              const SizedBox(height: 20),
-
-              // Info Card
-              Card(
-                elevation: 3,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  side: BorderSide(color: Colors.teal.shade200, width: 1.5),
-                ),
-
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Column(
-                    children: [
-                      ListTile(
-                        leading: Icon(Icons.phone, color: Colors.teal[700]),
-                        title: Text(
-                          "+92 300 1234567",
-                          style: GoogleFonts.poppins(fontSize: 16),
-                        ),
-                      ),
-                      const Divider(),
-                      ListTile(
-                        leading: Icon(Icons.email, color: Colors.teal[700]),
-                        title: Text(
-                          "customer@email.com",
-                          style: GoogleFonts.poppins(fontSize: 16),
-                        ),
-                      ),
-                    ],
+      //fetching data from firebase firestore
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .doc(currentUser?.uid)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final user = snapshot.data!.data() as Map<String, dynamic>;
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundColor: Colors.teal.shade200,
+                    child: Icon(Icons.person, size: 60, color: Colors.black),
                   ),
-                ),
+                  const SizedBox(height: 16),
+                  Text(
+                    user['name'] ?? 'No Name',
+
+                    style: GoogleFonts.poppins(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+
+                  // Info Card
+                  Card(
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      side: BorderSide(color: Colors.teal.shade200, width: 1.5),
+                    ),
+
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        children: [
+                          ListTile(
+                            leading: Icon(Icons.phone, color: Colors.teal[700]),
+                            title: Text(
+                              user['phone'] ?? 'No Phone',
+
+                              style: GoogleFonts.poppins(fontSize: 16),
+                            ),
+                          ),
+                          const Divider(),
+                          ListTile(
+                            leading: Icon(Icons.email, color: Colors.teal[700]),
+                            title: Text(
+                              user['email'] ?? 'no email',
+                              style: GoogleFonts.poppins(fontSize: 16),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
